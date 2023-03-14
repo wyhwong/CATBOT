b93a16c0486d794fc51c96879c43785513e492da
wyhwong <wyhwong.cuhk@gmail.com>
2023-03-15 01:12:31 +0800

    Binance client development

--- binance_stats_analyzer/utils/binance_client.py
+++ binance_stats_analyzer/utils/binance_client.py
@@ -16,39 +16,28 @@ class BinanceClient:
         self.duration_in_hrs = config["duration"]
         LOGGER.info("Initialized Binance client...")
 
-    def _get_historical_data(self, symbol: str, start_str: str, end_str: str, interval):
-        dataframe = self.client.get_historical_klines(
-            symbol=symbol, interval=interval, start_str=start_str, end_str=end_str
-        )
+    def _get_historical_data(self, symbol: str, start_str: str, interval):
+        dataframe = self.client.get_historical_klines(symbol=symbol, interval=interval, start_str=start_str)
         return pd.DataFrame(dataframe)
 
-    def get_klines(self, symbol: str, start_str: str, end_str: str, interval) -> pd.DataFrame:
-        dataframe = self._get_historical_data(
-            symbol=symbol, start_str=start_str, end_str=end_str, interval=interval
-        ).loc[:, :5]
+    def get_klines(self, symbol: str, start_str: str, interval: str) -> pd.DataFrame:
+        dataframe = self._get_historical_data(symbol=symbol, start_str=start_str, interval=interval).loc[:, :5]
         dataframe.columns = ["Time", "Open", "High", "Low", "Close", "Volume"]
         return dataframe.set_index(pd.to_datetime(dataframe["Time"], unit="ms")).astype(float)
 
-    def get_number_of_trade(self, symbol: str, start_str: str, end_str: str, interval) -> pd.DataFrame:
-        dataframe = self._get_historical_data(
-            symbol=symbol, start_str=start_str, end_str=end_str, interval=interval
-        ).loc[0:, (0, 8)]
+    def get_number_of_trade(self, symbol: str, start_str: str, interval) -> pd.DataFrame:
+        dataframe = self._get_historical_data(symbol=symbol, start_str=start_str, interval=interval).loc[0:, (0, 8)]
         dataframe.columns = ["Time", "Number_of_Trades"]
         return dataframe.set_index(pd.to_datetime(dataframe["Time"], unit="ms")).astype(float)
 
     def query(self, targets: list) -> tuple:
         kline_dataframes = []
         num_trades_dataframes = []
-        start_str = str(pd.Timestamp.now())
-        end_str = str(pd.Timestamp.now() - pd.Timedelta(hours=self.duration_in_hrs))
-        LOGGER.info(f"Querying data from Binance, {start_str=}, {end_str=}, {targets=}...")
+        start_str = (pd.Timestamp.now() - pd.Timedelta(days=30)).strftime("%Y-%m-%d' %H:%M:%S")
+        LOGGER.info(f"Querying data from Binance, {start_str=}, {targets=}...")
         for target in targets:
-            kline_dataframe = self.get_klines(
-                symbol=target, start_str=start_str, end_str=end_str, interval=self.interval
-            )
-            num_trades_dataframe = self.get_number_of_trade(
-                symbol=target, start_str=start_str, end_str=end_str, interval=self.interval
-            )
+            kline_dataframe = self.get_klines(symbol=target, start_str=start_str, interval=self.interval)
+            num_trades_dataframe = self.get_number_of_trade(symbol=target, start_str=start_str, interval=self.interval)
             kline_dataframes.append(kline_dataframe), num_trades_dataframes.append(num_trades_dataframe)
             LOGGER.info(f"Queried data of {target} from Binance.")
         return (kline_dataframes, num_trades_dataframes)
