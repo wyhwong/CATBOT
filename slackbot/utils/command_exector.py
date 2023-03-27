6a187c50345c520ffae35359cad780a5ebdd7fdc
wyhwong <wyhwong@link.cuhk.edu.hk>
2023-03-27 21:59:06 +0800

    Handle disconnection error in slack

--- slackbot/utils/command_exector.py
+++ slackbot/utils/command_exector.py
@@ -55,14 +55,20 @@ class SlackCommandExector:
         return message
 
     def _post_message(self, text: str, channel: str) -> None:
-        LOGGER.info(f"Sending message to Slack, channel: {channel}, text: {text}")
-        sent_message = self.web_client.chat_postMessage(channel=channel, text=text)["ok"]
-        LOGGER.info(f"Successfully sent message: {sent_message}")
+        LOGGER.debug(f"Sending message to Slack, channel: {channel}, text: {text}")
+        try:
+            sent_message = self.web_client.chat_postMessage(channel=channel, text=text)["ok"]
+            LOGGER.info(f"Successfully sent message: {sent_message}")
+        except Exception as err:
+            LOGGER.info(f"Failed to send message: {err}")
 
     def _post_attachment(self, title: str, file: str, channel: str) -> None:
-        LOGGER.info(f"Sending attachment to Slack, channel: {channel}, file: {file}")
-        sent_attachment = self.web_client.files_upload_v2(title=title, file=file, channel=channel)
-        LOGGER.info(f"Successfully sent attachment: {sent_attachment}")
+        LOGGER.debug(f"Sending attachment to Slack, channel: {channel}, file: {file}")
+        try:
+            sent_attachment = self.web_client.files_upload_v2(title=title, file=file, channel=channel)
+            LOGGER.info(f"Successfully sent attachment: {sent_attachment}")
+        except Exception as err:
+            LOGGER.info(f"Failed to send message: {err}")
 
     def help(self, text: str, user: str, channel: str) -> None:
         if text == "help":
